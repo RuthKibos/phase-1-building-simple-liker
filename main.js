e0e206c32e260d386664c14edf059474f040d2a2
RuthKibos <kibosruth@gmail.com>
2024-04-15 14:16:01 +0300

    Completed the lab

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", function() {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden"); // Add the .hidden class to hide the error modal initially
 
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  hearts.forEach(heart => {
+    heart.addEventListener("click", function() {
+      mimicServerCall()
+        .then(response => {
+          if (response === "Pretend remote server notified of action!") {
+            heart.classList.add("activated-heart");
+            if (heart.innerText === EMPTY_HEART) {
+              heart.innerText = FULL_HEART;
+            } else {
+              heart.innerText = EMPTY_HEART;
+            }
+          } else {
+            errorModal.innerText = response; // Display the server error message in the modal
+            errorModal.classList.remove("hidden"); // Remove the .hidden class to display the error modal
+            setTimeout(() => {
+              errorModal.classList.add("hidden"); // Hide the modal after 3 seconds
+            }, 3000);
+          }
+        })
+        })
+      })
+    })
+  
+  
+
+        
 
 
 //------------------------------------------------------------------------------
